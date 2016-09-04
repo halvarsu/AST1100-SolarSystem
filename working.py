4261b875cbec6b467572d204bf038327b26faeab
halvarsu <halvard.sutterud@gmail.com>
2016-09-04 16:59:43 +0200

    Working numerical integration and plot

---
+++ working.py
@@ -0,0 +1,77 @@
+# coding: utf-8
+from AST1100SolarSystem import AST1100SolarSystem
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def simulate(seed, System, years = 8,planet_index = 0, units='au'):
+    au =  149597870700          #m
+    sec_in_year = 365 * 24 * 3600      #s
+    G = 6.67408e-11 
+    solar_mass = 1.98855e30     #kg
+    n = 10000 *years
+    stop = years * sec_in_year 
+    dt = stop/n+1
+
+    starMass_kg = System.starMass * solar_mass
+    mass_kg = System.mass[planet_index] * solar_mass
+
+    x0 = System.x0 * au
+    y0 = System.y0 * au
+    vx0 = System.vx0 * au / sec_in_year
+    vy0 = System.vy0 * au / sec_in_year
+
+
+    times = np.linspace(0,stop,n)
+    pos = np.zeros((n+1, 2))
+    vel = np.zeros((n+1, 2))
+
+    pos[0] = np.array((x0[planet_index], y0[planet_index]))
+    vel[0] = np.array((vx0[planet_index], vy0[planet_index]))
+    accel = np.zeros((n+1, 2))
+    #accel[0] = calculate_forces(pos[0]) / mass_kg # second planet
+
+    r = np.sqrt(pos[0][0]**2 + pos[0][1]**2)
+    accel[0] = -G*starMass_kg*pos[0]/r**3
+
+    for i in range(len(times)): 
+        r = np.sqrt(pos[i][0]**2 + pos[i][1]**2)
+        accel[i] = -G*starMass_kg*pos[i]/r**3
+        #accel[i] =  calculate_forces(pos[i]) / mass_kg
+
+        vel[i+1] = vel[i] + accel[i] * dt
+        pos[i+1] = pos[i] + vel[i+1] * dt
+        #print accel[i], vel[i]
+
+    if units == 'au':
+        return pos/au
+    else:
+        return pos
+
+def planet_plotter(positions, years, fig=None, ax = None):
+    if not ax:
+        fig, ax = plt.subplots()
+    n = len(positions)
+    ax.plot(positions.T[0], positions.T[1])
+    colors = np.linspace(0,1,years+1)
+    ax.scatter(positions.T[0][::n/years], positions.T[1][::n/years], c=colors)
+    ax.axis('equal')
+    return fig, ax
+    
+
+if __name__ == "__main__":
+    seed = 87464
+    MyStarSystem = AST1100SolarSystem(seed) 
+    years = 30
+    fig, ax = plt.subplots()
+    ax.scatter(0,0, c='y')
+    for x in range(MyStarSystem.numberOfPlanets):
+        print x
+        positions = simulate(seed, MyStarSystem, planet_index = x,years=years)
+        fig, ax = planet_plotter(positions, years, fig, ax)
+
+    plt.axis('equal')
+    plot_max = max(MyStarSystem.a +1)
+    plt.axis([-plot_max,plot_max,-plot_max,plot_max])
+    plt.show()
