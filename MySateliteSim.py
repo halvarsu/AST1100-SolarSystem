c978b5d4cad9589571519a198b10ccd0f7b1a89f
halvarsu <halvard.sutterud@gmail.com>
2016-10-21 12:35:02 +0200

    moved some satelite-related functions to MySateliteSim.py

--- MySateliteSim.py
+++ MySateliteSim.py
@@ -18,6 +18,42 @@ def rot_matrix(angle):
     return np.array(((c,-s),(s,c)))
 
 
+def find_distance(pos_list, time = 0):
+    r_s = pos_list[-1]
+    rp_list = pos_list[:-1]
+    n = len(rp_list) 
+    print n
+    system = m.MySateliteSim(87464)
+
+    posFunc = system.getPositionsFunction()
+    planetPos = posFunc(time).T
+    print planetPos.shape
+
+    theta = np.linspace(0,2*np.pi, 10000)
+    circle = r_s* np.array((np.sin(theta), np.cos(theta)))
+    plt.plot(circle[0], circle[1],color = 'k')
+    for i in range(n):
+        x0 = planetPos[i,0]
+        y0 = planetPos[i,1]
+        rp = rp_list[i]
+        print '---------------------------'
+
+        A = (x0**2 + y0**2 + r_s**2 - rp**2)/(2*x0)
+        b = y0/float(x0)
+        C = np.sqrt(r_s**2*(b**2+1) - A**2)
+
+        y = [(A*b + C)/(b**2+1),(A*b - C)/(b**2+1)]
+        x = [A- y_i*b for y_i in y]
+        for i in range(2):
+            plt.scatter(x[i], y[i])
+            print "comp", x[i], y[i] 
+        circle = np.array((rp*np.sin(theta)+x0, rp*np.cos(theta)+y0))
+        plt.plot(circle[0], circle[1]) 
+    plt.scatter(0,0,c='y', s=100)
+    plt.axis('equal')
+    plt.show()
+
+
 def get_sat_velocity(dl_obs1, dl_obs2):
     '''Receives the doppler shift of the two reference stars and 
     returns the velocity of the spacecraft in the reference system 
