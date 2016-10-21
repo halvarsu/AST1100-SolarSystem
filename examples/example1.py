34c65ec9ee59b8a46c6c226e737bdf050e7cb0ea
halvarsu <halvard.sutterud@gmail.com>
2016-10-21 12:30:09 +0200

    added example

--- examples/example1.py
+++ examples/example1.py
@@ -3,11 +3,8 @@ import numpy as np
 
 system = m.MySateliteSim(87464)
 
-launch = True
-if launch:
-    system.launch_init()
-else:
-    system.boost_init(pos0 = (3,3), vel0 = (-3,3), t0 = 0)
+#system.launch_init()
+#system.boost_init(pos0 = (3,3), vel0 = (-3,3), t0 = 0)
 
 system.satelite_sim(speed_factor = [10,10,10])
 system.plot()
