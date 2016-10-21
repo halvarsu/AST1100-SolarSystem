9d5943adef0b89d00fdaa4cedab49b5c304887e1
halvarsu <halvard.sutterud@gmail.com>
2016-10-21 13:00:26 +0200

    Added figures

--- plot_exact.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-import numpy as np
-import matplotlib.pyplot as plt
-
-pos = np.load('positionsHomePlanet.npy')
-positions = pos.swapaxes(1,2)
-plt.plot(positions[0], positions[1])
-plt.axis('equal')
-plt.show()
